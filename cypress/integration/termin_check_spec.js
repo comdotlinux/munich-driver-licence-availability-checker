2fd56dc029c066ca545f20d4ef9b8dd6f8132bf0
Guruprasad Kulkarni <guru@linux.com>
2022-04-13 21:05:15 +0200

    Added Cypress and test

---
+++ cypress/integration/termin_check_spec.js
@@ -0,0 +1,18 @@
+// const { expect } = require("chai")
+// import chai, { expect } from "chai";
+
+describe('Check Divers License Availability', () => {
+	it('Only For Next Month!', () => {
+		cy.visit('https://terminvereinbarung.muenchen.de/fs/termin/index.php?loc=FS')
+		cy.contains('ausländischen Führerscheins').click()
+		cy.get('body > div.content > div.terminbuchung').get('#Umschreibung_SPACE_eines_SPACE_ausländischen_SPACE_Führerscheins > li > select').select('1')
+		cy.get('input.WEB_APPOINT_FORWARDBUTTON').click()
+		cy.get('div.nat_calendar_nav').get('a[href*="NEXT"]').click()
+		console.dir(cy.get('table.nat_calendar > tbody').get('td.nat_calendar').find('span'))
+		cy.get('table.nat_calendar > tbody').get('td.nat_calendar > span:nth-child(1)').each(($el, index, $list) => {
+			cy.wrap($el).should('contain.text', 'Keine freien Termine am')
+		})
+// #a6a84abc3c8666ca80a3655eef15bade20225 > table > tbody > tr:nth-child(3) > td:nth-child(5) > span:nth-child(1)
+
+	})
+  })
