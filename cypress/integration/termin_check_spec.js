51bd2c12c14e97b79cc124d82e57de0c092f6f76
Guruprasad Kulkarni <comdotlinux@users.noreply.github.com>
2022-04-18 22:28:38 +0000

    Checking both Current and Next Month

--- cypress/integration/termin_check_spec.js
+++ cypress/integration/termin_check_spec.js
@@ -4,8 +4,11 @@ describe('Check Divers License Availability', () => {
 		cy.contains('ausländischen Führerscheins').click()
 		cy.get('body > div.content > div.terminbuchung').get('#Umschreibung_SPACE_eines_SPACE_ausländischen_SPACE_Führerscheins > li > select').select('1')
 		cy.get('input.WEB_APPOINT_FORWARDBUTTON').click()
+		cy.get('table.nat_calendar > tbody').get('td.nat_calendar > span:nth-child(1)').each(($el, index, $list) => {
+			cy.wrap($el).should('not.be.visible').and('contain.text', 'Keine freien Termine am')
+			cy.wait(10)
+		})
 		cy.get('div.nat_calendar_nav').get('a[href*="NEXT"]').click()
-		console.dir(cy.get('table.nat_calendar > tbody').get('td.nat_calendar').find('span'))
 		cy.get('table.nat_calendar > tbody').get('td.nat_calendar > span:nth-child(1)').each(($el, index, $list) => {
 			cy.wrap($el).should('not.be.visible').and('contain.text', 'Keine freien Termine am')
 			cy.wait(10)
